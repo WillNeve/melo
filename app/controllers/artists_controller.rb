217f0a8f349cf5855b1d2ed46cd0f21f5020278e
William Neve <williamneve6000@gmail.com>
2022-11-28 17:56:44 +0000

    login page SKEL

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,14 @@
 class ArtistsController < ApplicationController
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.user = current_user
+    if @artist.save
+      redirect_to 
+    else
+    end
+  end
 end
