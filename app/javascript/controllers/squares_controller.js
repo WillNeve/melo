c5522dec06a303fd88728fc6709bc3c8a438b09e
William Neve <williamneve6000@gmail.com>
2022-11-29 23:41:12 +0000

    landing page magic

---
+++ app/javascript/controllers/squares_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="squares"
+export default class extends Controller {
+  static targets = ['squaresCont', 'login', 'signup', 'about', 'topLeft', 'topRight', 'blackBox', 'miniBlackBox']
+  connect() {
+    console.log('tiles connected')
+  }
+
+  aboutMode() {
+    if (this.squaresContTarget.classList.contains('about')) {
+      this.aboutTarget.innerHTML = 'About us'
+
+      this.blackBoxTarget.classList.toggle('active');
+      this.miniBlackBoxTarget.classList.toggle('active');
+
+      this.topLeftTarget.classList.toggle('shift')
+      this.topRightTarget.classList.toggle('shift')
+      setTimeout(() => {
+        this.squaresContTarget.classList.toggle('about')
+        setTimeout(() => {
+          this.loginTarget.classList.toggle('about')
+          this.signupTarget.classList.toggle('about')
+        }, 300);
+      }, 300);
+    } else {
+      this.aboutTarget.innerHTML = '<i class="fa-solid fa-arrow-left"></i>'
+
+      this.loginTarget.classList.toggle('about')
+      this.signupTarget.classList.toggle('about')
+      setTimeout(() => {
+        this.squaresContTarget.classList.toggle('about')
+      }, 150);
+      setTimeout(() => {
+        this.topLeftTarget.classList.toggle('shift')
+        this.topRightTarget.classList.toggle('shift')
+      }, 450);
+      setTimeout(() => {
+        this.blackBoxTarget.classList.toggle('active');
+        this.miniBlackBoxTarget.classList.toggle('active');
+      }, 750);
+    }
+  }
+}
