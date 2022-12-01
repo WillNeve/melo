bb16a29afdc048c76aed6b0ad80ac9b5fce27c2f
Tom Duchambon <tom.duchambon@gmail.com>
2022-12-01 12:19:59 +0000

    booking new implemented

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,8 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    users = [current_user, User.find(params[:id])]
     @targetuser = User.find(params[:id])
+    users = [current_user, @targetuser]
     @dm_room = dm_room.direct_message_for_users(users)
     @dm_roomall = dm_room.all
     @messages = @dm_room.messages.order(created_at: :desc).limit(100).reverse
