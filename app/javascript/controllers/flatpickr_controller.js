99a4b69c2fada849450b626c1c79deaabe819443
Tom Duchambon <tom.duchambon@gmail.com>
2022-12-02 13:13:29 +0000

    booking system

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+
+  static targets = [ "startTime", "endTime" ]
+
+  connect() {
+    flatpickr(this.startTimeTarget, {
+      enableTime: true,
+      // Provide an id for the plugin to work
+      plugins: [new rangePlugin({ input: "#end_time"})]})
+flatpickr(this.endTimeTarget, {})
+}
+}
