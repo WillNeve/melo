59b85a64142d93f57c7d690760f8f348913ee52a
William Neve <williamneve6000@gmail.com>
2022-12-06 01:16:47 +0000

    styling

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
-const context = require.context(".", true, /\.js$/)
-application.load(definitionsFromContext(context))
-
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
