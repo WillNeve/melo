b1ad40c196dfafd9f7e665e4d9ab3e57dc963a83
benjamin lescure <benjamin.lescure@wanadoo.fr>
2022-12-07 10:56:07 +0000

    adding toggle btn to search bar

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ['bubbleMenu', 'toggleButton']
+  static targets = ['bubbleMenu', 'toggleButton', 'search']
 
   connect() {
     console.log('navbar connected')
@@ -13,5 +13,9 @@ export default class extends Controller {
     console.log('avatar clicked')
     this.bubbleMenuTarget.classList.toggle('show')
     this.toggleButtonTarget.classList.toggle('active')
+  }
+
+  toggleSearch() {
+    this.searchTarget.classList.toggle('d-none');
   }
 }
